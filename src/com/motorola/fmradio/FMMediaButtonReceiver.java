899160079901901dd20b27fc7a85664bc3380429
Danny Baumann <dannybaumann@web.de>
2012-07-27 13:33:08 +0200

    Add double click feature for headset button.
    Single click mutes, double click advances to next preset or seeks next
    station, depending on the preference value.

--- src/com/motorola/fmradio/FMMediaButtonReceiver.java
+++ src/com/motorola/fmradio/FMMediaButtonReceiver.java
@@ -3,11 +3,52 @@ package com.motorola.fmradio;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.view.KeyEvent;
 import android.util.Log;
 
 public class FMMediaButtonReceiver extends BroadcastReceiver {
     private static final String TAG = "FMMediaButtonReceiver";
+
+    private static final int DOUBLE_CLICK_TIMEOUT = 500;
+    private static final int MSG_DOUBLE_CLICK_TIMEOUT = 1;
+
+    private static Handler sHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_DOUBLE_CLICK_TIMEOUT:
+                    Context context = (Context) msg.obj;
+                    String command = keycodeToCommand(msg.arg1);
+                    if (command != null) {
+                        startServiceForCommand(context, command);
+                    }
+                    break;
+            }
+        }
+    };
+
+    private static void startServiceForCommand(Context context, String command) {
+        Intent i = new Intent(context, FMRadioPlayerService.class);
+        i.setAction(FMRadioPlayerService.ACTION_FM_COMMAND);
+        i.putExtra(FMRadioPlayerService.EXTRA_COMMAND, command);
+        context.startService(i);
+    }
+
+    private static String keycodeToCommand(int keycode) {
+        switch (keycode) {
+            case KeyEvent.KEYCODE_HEADSETHOOK:
+            case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
+                return FMRadioPlayerService.COMMAND_TOGGLE_MUTE;
+            case KeyEvent.KEYCODE_MEDIA_NEXT:
+                return FMRadioPlayerService.COMMAND_NEXT;
+            case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
+                return FMRadioPlayerService.COMMAND_PREV;
+        }
+
+        return null;
+    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -22,29 +63,25 @@ public class FMMediaButtonReceiver extends BroadcastReceiver {
 
         int keycode = event.getKeyCode();
         int action = event.getAction();
+        long eventtime = event.getEventTime();
+        String command = keycodeToCommand(keycode);
+
+        Log.v(TAG, "Got media button event: action " + action +
+                " keycode " + keycode + " -> command " + command);
+
+        if (command == null) {
+            return;
+        }
 
         if (action == KeyEvent.ACTION_DOWN) {
-            String command = null;
-
-            switch (keycode) {
-                case KeyEvent.KEYCODE_HEADSETHOOK:
-                case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
-                    command = FMRadioPlayerService.COMMAND_TOGGLE_MUTE;
-                    break;
-                case KeyEvent.KEYCODE_MEDIA_NEXT:
-                    command = FMRadioPlayerService.COMMAND_NEXT;
-                    break;
-                case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
-                    command = FMRadioPlayerService.COMMAND_PREV;
-                    break;
-            }
-
-            Log.v(TAG, "Got media button down event: keycode " + keycode + " -> command " + command);
-            if (command != null) {
-                Intent i = new Intent(context, FMRadioPlayerService.class);
-                i.setAction(FMRadioPlayerService.ACTION_FM_COMMAND);
-                i.putExtra(FMRadioPlayerService.EXTRA_COMMAND, command);
-                context.startService(i);
+            if (keycode == KeyEvent.KEYCODE_HEADSETHOOK &&
+                    sHandler.hasMessages(MSG_DOUBLE_CLICK_TIMEOUT)) {
+                Log.v(TAG, "Detected double click of headset button, sending next command");
+                sHandler.removeMessages(MSG_DOUBLE_CLICK_TIMEOUT);
+                startServiceForCommand(context, FMRadioPlayerService.COMMAND_NEXT);
+            } else {
+                Message msg = sHandler.obtainMessage(MSG_DOUBLE_CLICK_TIMEOUT, keycode, 0, context);
+                sHandler.sendMessageDelayed(msg, DOUBLE_CLICK_TIMEOUT);
             }
         }
 
